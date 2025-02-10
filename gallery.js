9e69e1082f685d369720a3fd300deb4411ed3604
FotiosMpouris <126421111+FotiosMpouris@users.noreply.github.com>
2025-02-10 08:49:28 -0500

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,6 +3,7 @@
 // List your drawings in an array (newest first if you prefer).
 // Each entry is the filename in your 'drawings/' folder.
 const drawings = [
+  "drawing5.png",
   "drawing4.png",
   "drawing3.png",
   "drawing2.png",
