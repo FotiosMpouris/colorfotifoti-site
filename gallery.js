1540c8a37db32cb5f1f03a5e71db22039d0599cb
FotiosMpouris <126421111+FotiosMpouris@users.noreply.github.com>
2025-10-02 12:22:25 -0400

    feat: Add fotifoti34.jpg to gallery

--- gallery.js
+++ gallery.js
@@ -3,6 +3,7 @@
 // List your drawings in an array (newest first if you prefer).
 // Each entry is the filename in your 'drawings/' folder.
 const drawings = [
+  "fotifoti34.jpg",
   "fotifoti57.jpg",
   "fotifoti81.jpg",
   "fotifoti85.jpg",
