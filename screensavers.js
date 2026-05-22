1142312c9bf95a8d74e851110f5ad2e42840bc22
Fotios D. Mpouris <126421111+FotiosMpouris@users.noreply.github.com>
2026-05-22 12:20:42 -0400

    Add 1779466840-colorized.png to screensavers

--- screensavers.js
+++ screensavers.js
@@ -3,6 +3,7 @@
 // 1. List your colored images (screensavers). 
 // Newest first if you want.
 const screensavers = [
+  "1779466840-colorized.png",
   "1779207667-colorized.png",
   "1778948441-colorized.png",
   "1778689241-colorized.png",
